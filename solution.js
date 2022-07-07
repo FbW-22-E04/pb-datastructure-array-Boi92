506cee8ecc79bc330e2c16ff0ef04c1764b8b6b2
Boi Schrader <boi1992@t-online.de>
2022-07-07 13:07:43 +0200

    finished

---
+++ solution.js
@@ -0,0 +1,6 @@
+const myArray = ['apples', 'bananas', 'papaya', 'mango', 'cherry']
+console.log(myArray)
+var thirdElement = myArray[2]
+console.log(thirdElement)
+myArray [3] = 'fourth element'
+console.log(myArray)
